ba76d5a919982212a4f0a54b51995554d909fa38
DevelopMan <47355278+ControlWebManager@users.noreply.github.com>
2019-06-22 09:07:47 -0400

    Add files via upload

--- pos_lot_selection/models/models.py
+++ pos_lot_selection/models/models.py
@@ -5,11 +5,4 @@ from openerp import models, fields, api
 
 class PosSelectionStckoPiking(models.Model):
     _inherit='stock.quant'
-
-    #Realacion una a muchos obtenidad stock.production.lot, par obtener fecha de vencimiento , fecha de creacion
-
-    stock_production_lot_id = fields.Many2one('stock.production.lot', 'stock_quank')
-
-    use_date = fields.Datetime(related='stock_production_lot_id.use_date')
-    life_date = fields.Datetime(related='stock_production_lot_id.life_date')
-    creata_date = fields.Datetime(related='stock_production_lot_id.create_date', string="Fecha de Incorporacion")
+    pass
